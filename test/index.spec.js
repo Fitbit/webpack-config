8e70f91616ff58fb03e9798fe1888e088ceaf867
Marat Dreizin <marat.dreizin@gmail.com>
2015-10-13 15:27:45 +0300

    test(index): adds more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,8 +11,17 @@ var expect = require('expect.js'),
     ConfigPathResolver = require('../lib/configPathResolver');
 
 describe('Index', function () {
+    it('should be instance of "Config" and should have methods', function() {
+        var config = new Index();
+
+        expect(config).to.be.an(Config);
+
+        ['extend', 'merge', 'defaults', 'toObject', 'clone'].forEach(function(name) {
+            expect(config[name]).to.be.an(Function);
+        });
+    });
+
     it('should fill static properties', function() {
-        expect(new Index()).to.be.an(Config);
         expect(Index.nameResolver).to.be.an(ConfigNameResolver);
         expect(Index.factory).to.be.an(ConfigFactory);
         expect(Index.loader).to.be.an(ConfigLoader);
