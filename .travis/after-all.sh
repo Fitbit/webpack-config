7f18603a66a5a86dfe409c5b9cb4e4b43bfb0fc0
Marat Dreizin <mdreizin@fitbit.com>
2016-05-27 14:20:53 +0300

    chore(travis): removes `gitdown` task

--- .travis/after-all.sh
+++ .travis/after-all.sh
@@ -3,8 +3,7 @@ declare exitCode;
 $(npm bin)/travis-after-all
 exitCode=$?
 if [ $exitCode -eq 0 ]; then
-  npm run gitdown;
+  npm run semantic-release;
   npm run jsdoc;
   npm run gh-pages;
-  npm run semantic-release;
 fi
