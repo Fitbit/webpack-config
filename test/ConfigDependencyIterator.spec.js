2e772dfe52512e334e81f129d274defde51e7b61
Marat Dreizin <marat.dreizin@gmail.com>
2016-08-08 15:10:11 +0300

    test(ConfigDependencyIterator): adds missing tests

---
+++ test/ConfigDependencyIterator.spec.js
@@ -0,0 +1,39 @@
+import Config from '../src/Config';
+import ConfigDependency from '../src/ConfigDependency';
+import ConfigDependencyIterator from '../src/ConfigDependencyIterator';
+import MockConfigContainer from './helpers/MockConfigContainer';
+
+describe('ConfigDependencyIterator', () => {
+    let container = new MockConfigContainer(),
+        dependencyTree;
+
+    /**
+     * @private
+     * @param {String} filename
+     * @returns {Config}
+     */
+    const generateConfig = filename => container.resolve(Config).merge({ filename });
+
+    beforeEach(() => {
+        dependencyTree = new ConfigDependency(generateConfig('./test/fixtures/webpack.1.config.js'), [
+            new ConfigDependency(generateConfig('./test/fixtures/webpack.2.config.js')),
+            new ConfigDependency(generateConfig('./test/fixtures/webpack.3.config.js')),
+            new ConfigDependency(generateConfig('./test/fixtures/webpack.4.config.js'), [
+                new ConfigDependency(generateConfig('./test/fixtures/webpack.5.config.js'))
+            ])
+        ]);
+    });
+
+    describe('#next()', () => {
+        it('should go through `dependencyTree` recursively', () => {
+            const nodes = [],
+                iterator = new ConfigDependencyIterator(dependencyTree);
+
+            for (const {node} of iterator) {
+                nodes.push(node);
+            }
+
+            expect(nodes.length).toEqual(4);
+        });
+    });
+});
